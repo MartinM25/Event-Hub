854e20fd7925445c26e5a2e45dbc174513ec8ff1
MartinM25 <45079129+MartinM25@users.noreply.github.com>
2024-04-09 15:51:12 +0200

    Worked on the footer
    updated the footer, and added some footer links

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,14 +1,42 @@
+import { footerLinks, socialLinks } from "@/constants";
+import { Separator } from "../ui/separator"
 import Logo from "./Logo";
 import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-4 text-center sm:flex-row">
-        <Link href="/">
+    <footer className="border-t lg:px-20 pl-10 py-2">
+      <div className="text-center py-4">
+        <Link href="/" className="inline-block">
           <Logo />
         </Link>
-        <small>{new Date().getFullYear()} Event Hub. All rights Reserved</small>
+        <small className="block py-2">{new Date().getFullYear()} Event Hub. All rights Reserved</small>
+        {/* FOOTER NAV ITEMS */}
+        <ul className="flex flex-row gap-6 items-center justify-center">
+          {footerLinks.map((link) => {
+            return (
+              <li key={link.route} className="flex flex-row items-center gap-6 hover:underline">
+                <Link href={link.route}>
+                  <small>{link.label}</small> 
+                </Link>
+                <Separator orientation="vertical" className="h-5"/>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="flex items-center justify-center gap-x-6">
+        <Separator className="w-[200px] hidden md:block"/>
+        <ul className="flex items-center gap-x-5">
+          {socialLinks.map((social) => (
+            <li key={social.link}>
+              <Link href={social.link}>
+                <social.icon className="h-7 w-7 hover:text-primary duration-300" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Separator className="w-[200px] hidden md:block"/>
       </div>
     </footer>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,24 @@
+import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
+
+export const socialLinks = [
+  {
+    icon: Facebook,
+    link: "https://www.facebook.com/login.php/"
+  },
+  {
+    icon: Twitter,
+    link: "https://twitter.com/i/flow/login"
+  },
+  {
+    icon: Linkedin,
+    link: "https://www.linkedin.com/login"
+  },
+  {
+    icon: Instagram,
+    link: "https://www.instagram.com/"
+  },
+]
+
 export const headerLinks = [
   {
     label: 'Home',
@@ -9,6 +30,21 @@ export const headerLinks = [
   },
   {
     label: 'My Profile',
+    route: '/profile',
+  },
+]
+
+export const footerLinks = [
+  {
+    label: 'Events',
+    route: '/#events',
+  },
+  {
+    label: 'New Event',
+    route: '/events/create',
+  },
+  {
+    label: 'Profile',
     route: '/profile',
   },
 ]
@@ -25,3 +61,4 @@ export const eventDefaultValues = {
   isFree: false,
   url: '',
 }
+
