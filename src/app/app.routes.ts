8981cdedbe3db7ac547e86e62663a942cc802c54
Martin Manjoro <martin.manjoro25@gmail.com>
2025-04-21 13:45:04 +0200

    Routing Update
    - added the pages to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { EventDetailsComponent } from './pages/event-details/event-details.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'event/:id', component: EventDetailsComponent },
+  { path: 'create-event', component: CreateEventComponent },
+  { path: '**', component: NotFoundComponent }
+];
